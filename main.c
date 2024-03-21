4e06161ec43fba3d98a8b7c906c16cc4acc5f587
Gustavo Henrique <48604457+Tavinhuu@users.noreply.github.com>
2024-03-21 10:06:22 -0300

    Adicionado para perder dinheiro

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <locale.h>
 
 int escolhaMenuPrincipal, comecarapostas, cavaloaleatorio, apostaNoCavalo, back;
 
-int dinheiro = 0;
+int dinheiro = 1000;
+int inicio;
+int cavaloEscolhido = 0;
+
+// menuzinho
 
 char cavalo[] =
 
@@ -20,11 +25,12 @@ char cavalo[] =
 
 
 void menuprincipal(){
-    system("clear");
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     printf("%s", cavalo);
     printf("      -= Bem vindo ao sistema de apostas do Gustavo, escolha uma das opções\n");
     printf("      -= Digite 1 para checar seu saldo\n");
-    printf("      -= Digite 2 para começar suas apostas\n");
+    printf("      -= Digite 2 para começar suas apostas\n\n");
+    printf("      -= Digite qualquer outro valor para fechar\n");
     scanf("%d", &escolhaMenuPrincipal);
     
     switch(escolhaMenuPrincipal){
@@ -40,26 +46,74 @@ void menuprincipal(){
         case 2:
             menuCavalo();
             break;
+        default:
+            printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+            printf("%s", cavalo);
+            printf("      -=  Obrigado por utilizar");
     }
     
 }
 
 void menuCavalo(){
-    system("clear");
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     printf("%s", cavalo);
     printf("      -= Legal! Vamos começar com apostas em cavalos?, escolha uma das opções\n");
-    printf("      -= Digite 1 para comecar as apostas\n");
+    printf("      -= Digite 1 para comecar as apostas\n\n");
+    printf("      -= Digite qualquer número para retornar ao menu\n");
     scanf("%d", &comecarapostas);
-    switch(comecarapostas){
-        case 1:
-            comecarCorrida();
-    
+    if(comecarapostas == 1){
+        if (dinheiro >= 10){
+              comecarCorrida();
+        } else {
+          printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+          printf("%s", cavalo);
+          printf("      -= Ah!! Que pena, você não tem dinheiro suficiente");
+        }
             
-        case 2:
-            menuprincipal();
-            
+    } else {
+        menuprincipal();
     }
 }
+
+void comecarCorrida(){
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    printf("%s", cavalo);
+    
+    printf("      -=  Escolha seu cavalo para apostar\n");
+    printf("\n      -= Trovão Veloz - garanhão majestoso conhecido por sua velocidade imbatível");
+    printf("\n      -= Estrela Dourada - conhecida por sua graça e habilidade nas curvas");
+    printf("\n      -= Flecha Rápida - um cavalo ágil e astuto, capaz de acelerar como uma flecha");
+    printf("\n      -= Coração Valente - conhecido por nunca desistir, mesmo nas condições mais desafiadoras da pista\n");
+            
+    cavaloaleatorio = rand() % 2;
+            
+    scanf("%d", &apostaNoCavalo);
+    switch(apostaNoCavalo){
+        case 1:
+            cavaloEscolhido = 1;
+            corridaAndamento();
+            
+            break;
+        
+        case 2:
+            cavaloEscolhido = 2;
+            corridaAndamento();
+            
+            break;
+        case 3:
+            cavaloEscolhido = 3;
+
+            corridaAndamento();
+            
+            break;
+        case 4:
+            cavaloEscolhido = 4;
+            corridaAndamento();
+            
+            break;
+}
+}
+
 
 
 void corridaAndamento(){
@@ -81,12 +135,15 @@ void corridaAndamento(){
     return mensagens[indice];
 }
 
-    int inicio;
-    system("clear");
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     printf("      -=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=\n");
     printf("      -= A corrida foi iniciada! Seu cavalo é o N%d    -=\n", apostaNoCavalo );
     printf("      -=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=\n\n");
+    // espera 2 seg pra enviar as msg
     sleep(3);
+
+    //envia as msg aleatoria em loop com delay de 2 seg por 6 vezes
+
     for(inicio = 0; inicio<6; inicio++){
         srand(time(NULL));
         char *mensagem = escolherMensagem();
@@ -94,47 +151,107 @@ void corridaAndamento(){
         sleep(3);
     }
     
+    // ver o vencedor aleatorio
+
     srand(time(NULL));
     int vencedoraleatorio = rand() % 3;
     
     sleep(1);
     printf("      -= O vencedor foi o cavalo N%d\n", vencedoraleatorio+1);
-    if (apostaNoCavalo == 1){
+
+    // caso escolha o cavalo 1
+
+
+    if (cavaloEscolhido == 1){
+
+        // se o vencedor for o cavalo 1
+
         if (vencedoraleatorio == 0) {
+
+            // envia para o menu de win
+
             voceVenceu();
+
+            // se o vencedor não for o 1
+
+        } else{
+
+            // perdeu fi
+
+            vocePerdeu();
         }
     }
-    if (apostaNoCavalo == 2){
+    if (cavaloEscolhido == 2){
         if (vencedoraleatorio == 1) {
             voceVenceu();
+        } else{
+            vocePerdeu();
         }
     }
-    if (apostaNoCavalo == 3){
+    if (cavaloEscolhido == 3){
         if (vencedoraleatorio == 2) {
             voceVenceu();
+        } else{
+            vocePerdeu();
         }
     }
-    if (apostaNoCavalo == 4){
+    if (cavaloEscolhido == 4){
         if (vencedoraleatorio == 3) {
             voceVenceu();
+        } else{
+            vocePerdeu();
         }
     }
+
+    //fiz isso aq so pra voltar pro menu tbm pq tava voltando direto
+
     int nada;
     scanf("%d", &nada);
         
     
 }
 
+// se ele vencer
+
 
 void voceVenceu(){
     int voltarmenu;
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     printf("%s", cavalo);
     printf("      -= Legal! Você venceu! Foram adicionados $10,00 na sua conta\n");
     
+    // adiciona 10
+
     dinheiro += 10;
     
     printf("      -= Digite qualquer número para retornar ao menu\n");
     scanf("%d", &voltarmenu);
+
+    // volta pro menu
+
+    if (voltarmenu != 0){
+        menuprincipal();
+    }
+}
+
+
+// Caso o usuário não ganhe este será o menu
+
+void vocePerdeu(){
+    int voltarmenu;
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    printf("%s", cavalo);
+    printf("      -= Ah que pena, você perdeu! Foram removidos $10,00 na sua conta\n");
+    
+    // remove 10 de dinheiro
+
+    dinheiro -= 10;
+    
+    printf("      -= Digite qualquer número para retornar ao menu\n");
+    scanf("%d", &voltarmenu);
+
+    // retorna ao menu
+
     if (voltarmenu != 0){
         menuprincipal();
     }
@@ -142,41 +259,8 @@ void voceVenceu(){
 
 
 
-void comecarCorrida(){
-    printf("%s", cavalo);
-    
-    printf("      -=  Escolha seu cavalo para apostar\n");
-    printf("\n      -= Trovão Veloz - garanhão majestoso conhecido por sua velocidade imbatível");
-    printf("\n      -= Estrela Dourada - conhecida por sua graça e habilidade nas curvas");
-    printf("\n      -= Flecha Rápida - um cavalo ágil e astuto, capaz de acelerar como uma flecha");
-    printf("\n      -= Coração Valente - conhecido por nunca desistir, mesmo nas condições mais desafiadoras da pista\n");
-            
-    cavaloaleatorio = rand() % 2;
-            
-    scanf("%d", &apostaNoCavalo);
-    switch(apostaNoCavalo){
-        case 1:
-            corridaAndamento();
-            
-            break;
-        
-        case 2:
-            corridaAndamento();
-            
-            break;
-        case 3:
-            corridaAndamento();
-            
-            break;
-        case 4:
-            corridaAndamento();
-            
-            break;
-}
-}
-
-
 
 int main(void){
+    setlocale(LC_ALL, "Portuguese");
     menuprincipal();
 }
